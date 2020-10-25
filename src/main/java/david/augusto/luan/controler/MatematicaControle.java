673ad550256fd45c075c5163a612b9a82afa5e37
luan-alencar <luanalencar134@gmail.com>
2020-10-25 14:32:36 -0300

    metodo de soma passando

--- src/main/java/david/augusto/luan/controler/MatematicaControle.java
+++ src/main/java/david/augusto/luan/controler/MatematicaControle.java
@@ -1,7 +1,5 @@
 package david.augusto.luan.controler;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MatematicaControle {
 
-	private static final String template = "Hello, %s!";
-
-	private final AtomicLong counter = new AtomicLong();
-
-	@RequestMapping(value = "/soma/numeroUm/numeroDois", method = RequestMethod.GET)
+	@RequestMapping(value = "/soma/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
 	public Double soma(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois)
 			throws Exception {
 
@@ -25,11 +19,19 @@ public class MatematicaControle {
 		return soma;
 	}
 
-	private Double converterParaDouble(String numeroDois) {
+	private Double converterParaDouble(String strNumero) {
+		if (strNumero == null)
+			return 0D;
+		String numero = strNumero.replaceAll(",", ".");
+		if (isNumero(numero))
+			return Double.parseDouble(numero);
 		return 1D;
 	}
 
-	private boolean isNumero(String numero) {
-		return false;
+	private boolean isNumero(String strNumero) {
+		if (strNumero == null)
+			return false;
+		String numero = strNumero.replaceAll(",", ".");
+		return numero.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
