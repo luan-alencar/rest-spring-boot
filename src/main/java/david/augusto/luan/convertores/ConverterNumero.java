71fc0b96f998d3c79bf06c5d1ddb693d9f726ea3
luan-alencar <luanalencar134@gmail.com>
2020-10-25 17:38:24 -0300

    adicionando metodos nessa classe para simplificar MathControler

--- src/main/java/david/augusto/luan/convertores/ConverterNumero.java
+++ src/main/java/david/augusto/luan/convertores/ConverterNumero.java
@@ -1,15 +1,21 @@
 package david.augusto.luan.convertores;
 
-import david.augusto.luan.utils.IsNumero;
-
 public class ConverterNumero {
 
 	public static Double converterParaDouble(String strNumero) {
 		if (strNumero == null)
 			return 0D;
 		String numero = strNumero.replaceAll(",", ".");
-		if (IsNumero.isNumero(numero))
+		if (isNumero(numero))
 			return Double.parseDouble(numero);
 		return 0D;
 	}
+
+	public static boolean isNumero(String strNumero) {
+		if (strNumero == null)
+			return false;
+		String numero = strNumero.replaceAll(",", ".");
+		return numero.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+
 }
