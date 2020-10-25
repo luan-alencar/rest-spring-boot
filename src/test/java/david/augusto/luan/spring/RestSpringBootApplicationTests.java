4fd04ea22b17fb179ee2de75fe604b19035b2ed6
luan-alencar <luanalencar134@gmail.com>
2020-10-25 11:58:04 -0300

    primeiro commit

---
+++ src/test/java/david/augusto/luan/spring/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package david.augusto.luan.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
