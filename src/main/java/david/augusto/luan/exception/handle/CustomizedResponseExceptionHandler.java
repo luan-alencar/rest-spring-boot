edc32d674882ef89c5cd08ba1b65b32ce50087b7
luan-alencar <luanalencar134@gmail.com>
2020-10-25 16:15:56 -0300

    refatorando classe

--- src/main/java/david/augusto/luan/exception/handle/CustomizedResponseExceptionHandler.java
+++ src/main/java/david/augusto/luan/exception/handle/CustomizedResponseExceptionHandler.java
@@ -5,19 +5,31 @@ import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import david.augusto.luan.exception.ExceptionResponse;
+import david.augusto.luan.exception.UnsuportedMathOperationException;
 
 @ControllerAdvice
-@RestControllerAdvice
-public class CustomizedResponseExceptionHandler {
+@RestController
+public class CustomizedResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
 		// a excecao mais generica em um servico rest e o INTERNAL_SERVER_ERROR
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UnsuportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		// a excecao mais generica em um servico rest e o INTERNAL_SERVER_ERROR
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 }
